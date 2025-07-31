908fed72632fda495fd7c88ab2fef98be0768158
Soumita <soumita.dalal@gmail.com>
2025-08-01 01:06:07 +0530

    dudu bubu msg

--- gui.py
+++ gui.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import pandas as pd
 
-st.write("hello")
+st.title("Give Your Inputs")
+
+with st.form(key="user_info_form"):
+    button=None
+    st.subheader("Bubu loves Dudu?")
+    lv=st.radio("choose option my pookiee boo!!",['None','Yes','No'])
+    st.subheader("Dudu is always right?")
+    rd=st.radio("choose option Bubu",['None','Yes','No'])
+    st.subheader("Dudu will order biriyani for Bubu?")
+    box=st.selectbox("choose option if you are hungry Bubu",['None','Yes','No'])
+
+    button=st.form_submit_button()
+    if button:
+        if lv.lower() == 'yes' and rd.lower()=='yes' and box.lower()=='yes':
+            st.markdown("Dudu is happy!")
+        else:
+            st.write('Dudu is sad :(')
+            st.warning('Choose Wisely Bubu!!!')
+    else:
+        st.write('No Message for Bubu')
+        
+
